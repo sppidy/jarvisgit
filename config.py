f4a3ae90109e7564878f513dedaa7a3611a2a5fc
J.A.R.V.I.S <creatorofjarvis@gmail.com>
2020-08-22 13:29:23 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,8 +1,8 @@
 # Your Bot token in the quotes taken from @BotFather
-BOT_TOKEN = "1158822364:AAEgnhKPvoTbPU5KnO8iMYqQfuF_XcxbBUE"
+BOT_TOKEN = "1311212004:AAHolSOzfMPJSpZ7lDqdyG1f24QpjplQvSw"
 
 # Project Name of your Project
-PROJECT_NAME = "GitGram"
+PROJECT_NAME = "JARVIS"
 
 # If you forked the source code, grab your repo's link and paste it below!
-GIT_REPO_URL = "https://github.com/MadeByThePinsHub/GitGram"
+GIT_REPO_URL = "https://github.com/jarvis210904/jarvisgit"
